8e880bff3fa51b13ff505607a8b11823444acc32
halil <halilsafayav@gmail.com>
2025-06-22 14:51:19 +0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'https://api.harmanagel.com', // Production backend URL'iniz
   oauth: {
     issuer: 'https://api.harmanagel.com', // Production issuer URL
-    clientId: 'HarmanaGel_App',
+    clientId: 'HarmanaGel_Swagger_Ui',
     redirectUri: 'https://harmanagel.com', // Production frontend URL
     postLogoutRedirectUri: 'https://harmanagel.com',
     scope: 'openid profile email phone roles HarmanaGel',
